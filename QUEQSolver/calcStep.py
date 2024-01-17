e09a64a2d2f185d7a9b181b2846b8340580dce48
josef223 <a00300537@unet.univie.ac.at>
2024-01-17 21:10:20 +0000

    package

---
+++ QUEQSolver/calcStep.py
@@ -0,0 +1,118 @@
+# Calculate quadratic eqation, step by step and print all steps
+# last modified: 4.11.2023
+
+import numpy as np
+import sympy as sp
+from pydantic import BaseModel, field_validator, ConfigDict
+from typing import Union
+
+def is_numeric(value):
+    '''
+    Function to check if the value is numeric (int, float or complex)
+    If value is a string it returns False.
+
+    Args:
+        value (any type): input variable that is checked, any type
+
+    Returns:
+        bool: True if value is numeric (int, float or complex)
+    '''
+    return isinstance(value, (int, float, complex))
+
+
+def is_numeric_or_complex(value):
+    '''
+    Function checks if a value can be converted to a complex number
+    If value is a string it returns False.
+
+    Args:
+        value (any type): input variable that is checked, any type
+
+    Returns:
+        bool: True if the value can be converted to complex datatype
+    '''
+
+    try:
+        complex(value)
+    except (ValueError, TypeError):
+        # The conversion failed, so it's not a numeric input
+        return False
+    else:
+        # The conversion succeeded, so it's a numeric input
+        return True
+
+class Coefficients(BaseModel):
+     a: Union[str, float, complex, int]
+     b: Union[str, float, complex, int]
+     c: Union[str, float, complex, int]
+     model_config = ConfigDict(arbitrary_types_allowed=True, extra='ignore')
+
+def quadraticEQSolver(a,b,c):
+    '''
+    Function solved a quadratic equation and prints all steps
+
+    Args:
+        a, b, c (Union[str, float, complex, int]): input coefficients of the quadratic equation
+
+    Returns:
+        tuple: A tuple containing a boolean if the equation is plotable, the roots x1 and x2 as well as the coefficients a, b, c
+    '''
+    coeffs = Coefficients(a=a, b=b, c=c)
+    # Use validated coefficients
+    a, b, c = coeffs.a, coeffs.b, coeffs.c
+##############################################
+    # Assign values as strings
+    #a=5; b=8; c=3
+    #a=1; b=3; c=8
+    #a=1; b='p'; c='q'
+    print('QUADRATIC EQUATION SOLVER:')
+    #c=1+2j ####################
+    print(f'solving: {a}*x^2+{b}*x+{c}=0\n')
+
+    # Calculate p and q using sympy for symbolic computation
+    a_sym, b_sym, c_sym = sp.symbols('a b c')
+    p = b_sym / a_sym
+    q = c_sym / a_sym
+
+    # Create symbolic expressions for p and q
+    p_expr = p.subs({a_sym: a, b_sym: b})
+    q_expr = q.subs({a_sym: a, c_sym: c})
+
+    plotable=False
+    if (is_numeric_or_complex(a) and is_numeric_or_complex(b) and is_numeric_or_complex(c)):
+        a=complex(a); b=complex(b); c=complex(c)
+        if a.imag==0 and b.imag==0 and c.imag==0:
+            a=float(a.real); b=float(b.real); c=float(c.real)
+        p=b/a; q=c/a
+        p_expr=p
+        q_expr=q
+        plotable=True
+
+    print(f'p = b/a = {p_expr}')
+    print(f'q = c/a = {q_expr}\n')
+
+    # Printing the expressions
+    tab=40
+    YELLOW = '\033[93m'; RESET = '\033[0m'
+    print(f'x^2+{p_expr}*x+{q_expr}=0'.ljust(tab), f'{YELLOW}/-{q_expr} | on both sides{RESET}')
+    print(f'x^2+{p_expr}*x={-q_expr}'.ljust(tab), f'{YELLOW}/+(p/2)^2 | completing the square\n{RESET}')
+
+    print(f'x^2 + 2*{p_expr}/2*x + ({p_expr}/2)^2 = ({p_expr}/2)^2 - {q_expr}'.ljust(tab),f'{YELLOW}| bionomial formula{RESET}')
+    print(f'(x+{p_expr}/2)^2 = ({p_expr}/2)^2 - {q_expr}'.ljust(tab), f'{YELLOW}| sqrt() on both sides{RESET}')
+    print(f'|x+{p_expr}/2| = sqrt( ({p_expr}/2)^2 - {q_expr} )'.ljust(tab), f'{YELLOW}| next solve absolute value{RESET}')
+    print(f'x+{p_expr}/2 = \u00B1sqrt( ({p_expr}/2)^2 - {q_expr} )'.ljust(tab), f'{YELLOW}/ -{p_expr}/2 | subtract p/2 from both sides{RESET}')
+    print(f'x = {-p_expr}/2 \u00B1 sqrt( ({p_expr}/2)^2 - {q_expr} )\n')
+
+    # Evaluate the quadratic equation for x1 and x2
+    x_sym = sp.symbols('x')
+    equation = sp.Eq(x_sym**2 + p_expr*x_sym + q_expr, 0)
+    solutions = sp.solve(equation, x_sym)
+    if len(solutions) == 1:
+        # Only one solution (repeated root)
+        x1 = x2 = solutions[0]
+    else:
+        x2, x1 = solutions
+
+    print(f'x1 = {x1.evalf()} \nx2 = {x2.evalf()}')
+    return plotable,x1,x2,a,b,c
+
---
+++ QUEQSolver/qesolver.py
@@ -0,0 +1,116 @@
+#QUEQSolver: Quadratic Equation Solver that prints all steps and plots the results
+'''
+- Values can be provided from command line by entering a, b, c as arguments
+-  If argument -f is entered, the values are read from values.txt
+- If it is started without an argument, the user input is started
+- Also alphanumeric values can be entered as a value, not only float
+- The final values are shown as an output
+
+last modified: 4.11.2023
+'''
+import sys
+from QUEQSolver.calcStep import quadraticEQSolver
+from QUEQSolver.plotQE import QEplot
+
+# Pydantic:
+from pydantic import FilePath
+from pydantic import BaseModel, field_validator, ConfigDict
+from typing import Union
+
+def read_file(filename: FilePath):
+    '''
+    Function reads values from the specified file
+
+    Description:
+        The first line is skipped because it contains the header. Then it  parses each line, splitting it by '#' and stripping off all additional whitespaces such that the numerical value can be extracted.
+
+    Args:
+        filename (FilePath): name of the file to read
+
+    Returns:
+        list: A list of values read from the file
+    '''
+    values=[]
+    try:
+        with open(filename, 'r') as file:
+            next(file); #skip one line as header
+            for line in file:
+                number=line.split('#')[0].strip()
+                values.append(number)
+            return values
+    except StopIteration:
+        raise ValueError("The file does not have the expected format")
+def inputValues():
+    '''
+    Read the input from file, command-line or keyboard input
+
+    Description: 3 different input Methods:
+         - If the 3 arguments are passed via command line, they are used
+         - If the '-f' flag is passed with a filename, it reads the coefficient from the specified file
+         - If there are no valid command-line inputs, it prompts the user to enter the coefficients manually.
+         - It replaces 'i', 'I', or 'J' with 'j' to support different complex number notations
+         - It is able to handle errors like 'file not found' or 'unexpected errors'
+
+    Args:
+        None
+
+    Returns:
+        tuple: containing three values (a,b,c) representing the coefficients of a quadratic equation
+    '''
+    try:
+        if len(sys.argv) == 4:
+            a, b, c = sys.argv[1], sys.argv[2], sys.argv[3]
+        else:
+            if len(sys.argv) == 2 and sys.argv[1]=='-f':
+                print('reading values from file: values.txt')
+                a, b, c = read_file('values.txt')
+            elif len(sys.argv) == 3 and sys.argv[1]=='-f':
+                filename = sys.argv[2]
+                print(f'reading values from file: {filename}')
+                a, b, c = read_file(filename)
+            else:
+                print('use -f command to read values from file')
+                print('less than 3 arguments provided in command line: starting input\n')
+                print('user input:')
+                a = input("a = ")
+                b = input("b = ")
+                c = input("c = ")
+        a = a.replace('i', 'j').replace('I', 'j').replace('J', 'j')
+        b = b.replace('i', 'j').replace('I', 'j').replace('J', 'j')
+        c = c.replace('i', 'j').replace('I', 'j').replace('J', 'j')
+        print(f"values: a = {a}, b = {b}, c = {c}")
+        return a,b,c
+    # Print error messages:
+    except FileNotFoundError as e:
+        print("Error: The file values.txt was not found.")
+    except Exception as e:
+        print(f"An unexpected error occurred: {e}")
+
+####### Pydantic for run_quadraticEQSolver ########
+class Coefficients(BaseModel):
+    a: Union[str, float, int, complex]
+    b: Union[str, float, int, complex]
+    c: Union[str, float, int, complex]
+    model_config = ConfigDict(arbitrary_types_allowed=True, extra='ignore')
+
+def run_quadraticEQSolver(a,b,c):
+    '''
+    Function runs the quadratic equation solver directly
+
+    Args:
+        a, b, c (int, float, str, complex): input coefficients of the quadratic equation
+    '''
+    coeffs = Coefficients(a=a, b=b, c=c)
+    plotable,x1,x2,a,b,c=quadraticEQSolver(coeffs.a,coeffs.b,coeffs.c)
+    if plotable:
+        QEplot(a,b,c,x1,x2)
+
+def main():
+    [a,b,c]=inputValues()
+    plotable,x1,x2,a,b,c=quadraticEQSolver(a,b,c)
+    if plotable:
+        QEplot(a,b,c,x1,x2)
+
+if __name__ == "__main__":
+    main()
+
